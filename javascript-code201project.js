2c0b929fd53bb7e57df9d6605d13cfad1a6147f0
davidyueone <131464525+davidyueone@users.noreply.github.com>
2023-05-14 14:03:25 +0000

    Add files via upload

--- javascript-code201project.js
+++ javascript-code201project.js
@@ -2,23 +2,21 @@
 let loginForm = document.getElementById("loginForm");
 
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+e.preventDefault();
 
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 
-if (username.value = "" || password.value = "")
-{
+if (username.value == "" || password.value == "") {
 alert("Ensure you input a value in both fields!");
-}
-else {
+} else {
 // perform operation with form input
-alert("This form has been submitted!");
-console.log("This form has a username of ${username.value} and a password of ${password.value}"
+alert("This form has been successfully submitted!");
+console.log(
+  `This form has a username of ${username.value} and password of ${password.value}`
 );
 
 username.value = "";
 password.value = "";
-
 }
 });
