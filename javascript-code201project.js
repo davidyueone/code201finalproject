77ef585b07a2af47f0c438cd5b3d3f7a4316714f
davidyueone <131464525+davidyueone@users.noreply.github.com>
2023-05-14 15:51:58 +0000

    Add files via upload

--- javascript-code201project.js
+++ javascript-code201project.js
@@ -8,11 +8,11 @@ let username = document.getElementById("username");
 let password = document.getElementById("password");
 
 if (username.value == "" || password.value == "") {
-alert('Ensure you input a value in both fields!');
+alert("Ensure you input a value in both fields!");
 } else {
 // perform operation with form input
-alert('This form has been successfully submitted!');
-console.log('This form has a username of ${username.value} and password of ${password.value}');
+alert("This form has been successfully submitted!");
+console.log("This form has a username of ${username.value} and password of ${password.value}");
 
 username.value = "";
 password.value = "";
